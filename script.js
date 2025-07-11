10a72ee2cd63eb9221574e32d5cd75f5e8dc9eb5
A.ASHWIN KUMAR <119407186+ASHWINKUMAR2903@users.noreply.github.com>
2025-07-11 22:42:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+async function getWeather() {
+  const city = document.getElementById("cityInput").value.trim();
+  const output = document.getElementById("weatherOutput");
+
+    if (!city) 
+    {
+        output.innerText = "Enter the city name.";
+        return;
+    }
+
+  output.innerText = "Fetching weather...";
+
+  let url = `https://api.weatherapi.com/v1/current.json?key=fa2090f83d5e4bd4925164631251107&q=${city}`
+
+  try 
+  {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("City not found or API limit exceeded.");
+    }
+
+    const data = await response.json();
+
+    const location = `${data.location.name}, ${data.location.country}`;
+    const temperature = `${data.current.temp_c} °C`;
+    const condition = data.current.condition.text;
+    const icon = data.current.condition.icon;
+
+    output.innerHTML = 
+    `
+      <strong>Location:</strong> ${location}<br>
+      <strong>Temperature:</strong> ${temperature}<br>
+      <strong>Condition:</strong> ${condition}<br>
+      <img src="https:${icon}" alt="${condition}" />
+    `;
+  } 
+  catch (error) 
+  {
+    output.innerText = `Error: ${error.message}`;
+    console.error(error);
+  }
+}
